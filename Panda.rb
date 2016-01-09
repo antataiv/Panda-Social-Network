25df0fcced5da1df5e46c24b2e4db00fc9b7674c
Your Name <forthewin.0000156@gmail.com>
2016-01-09 13:42:37 +0200

    add alias_method

--- Panda.rb
+++ Panda.rb
@@ -7,6 +7,10 @@ class Panda
     @gender = gender
   end
 
+  def ==(other)
+    to_s == other.to_s
+  end
+
   def male?
     gender == "male"
   end
@@ -16,16 +20,23 @@ class Panda
   end
 
   def to_s
-    "#{name} is a #{gender} panda with email: #{email}"
+    "#{@name} is a #{@gender} panda with email: #{@email}"
   end
 
   def hash
     to_s.hash
   end
+
+alias_method :eql?, :==
+
 end
 
-#ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
-#ivo2 = Panda.new("Ivo", "ivo@pandamail.com", "male")
+ivo = Panda.new("Ivo", "ivo@pandamail.com", "male")
+ivo2 = Panda.new("Ivo", "ivo@pandamail.com", "male")
+ivo3 = Panda.new("ivo","ivo@abv.bg", "male")
+
+puts ivo == ivo3
+puts ivo.hash == ivo2.hash
 #
 #puts ivo.hash
 #puts ivo2.hash
